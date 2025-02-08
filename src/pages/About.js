59c77afc56c675dd158dc3022c62808d63edc5f0
guilhermebrito123 <gbfsilva@sga.pucminas.br>
2025-02-08 20:31:30 -0300

    Adicionando logo no componente About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,9 @@
 const About = () => {
   return (
-    <h2 style={{marginTop: 50}}>Somos uma empresa focada na distribuição de artigos de qualidade voltados para área esportiva.</h2>
+    <div>
+      <h2 style={{marginTop: 50}}>Somos uma empresa focada na distribuição de artigos de qualidade voltados para área esportiva.</h2>
+      <img src="https://logowik.com/content/uploads/images/ea-sports-fc9968.logowik.com.webp" alt="Foto" />
+    </div>    
   )
 }
 
